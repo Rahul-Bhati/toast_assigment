c2c9b899069d3cb3a2049d7a2cf3e95fd30b131f
Rahul-Bhati <rb083380@gmail.com>
2024-06-17 10:11:45 +0530

    add new toast coponent

--- src/component/toast/ToastComponent.jsx
+++ src/component/toast/ToastComponent.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import './ToastComponent.css';
 
-const ToastComponent = ({ onConfirm }) => {
+const ToastComponent = (props) => {
+  const { onConfirm, toastDetail } = props;
+
   return (
-    <div className="toast-container">
-      <div className="toast">
+    <div className="toast-container" >
+      <div className="toast" style={{background:"white" , color:"black"}}>
         <div className="toast-header">
-          <h3 className="toast-title">Title</h3>
           <button className="toast-close" onClick={onConfirm}>
             X
           </button>
         </div>
         <div className="toast-body">
-          <p>This is a toast message</p>
-        </div>
-        <div className="toast-footer">
-          <button className="confirm-button" onClick={onConfirm}>
-            Confirm
-          </button>
-          <button className="toast-button" onClick={onConfirm}>
-            Cancel
-          </button>
+          <img src="check.svg" alt={toastDetail.img} className={`filter-${toastDetail.bg}`}/>
+          <p>{toastDetail.text}</p>
         </div>
       </div>
     </div>
@@ -28,3 +22,35 @@ const ToastComponent = ({ onConfirm }) => {
 };
 
 export default ToastComponent;
+
+
+// import React from 'react';
+// import './ToastComponent.css';
+
+// const ToastComponent = ({ onConfirm }) => {
+//   return (
+//     <div className="toast-container">
+//       <div className="toast">
+//         <div className="toast-header">
+//           <h3 className="toast-title">Title</h3>
+//           <button className="toast-close" onClick={onConfirm}>
+//             X
+//           </button>
+//         </div>
+//         <div className="toast-body">
+//           <p>This is a toast message</p>
+//         </div>
+//         <div className="toast-footer">
+//           <button className="confirm-button" onClick={onConfirm}>
+//             Confirm
+//           </button>
+//           <button className="toast-button" onClick={onConfirm}>
+//             Cancel
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ToastComponent;
